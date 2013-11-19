5345797d26dea71ef57508e510135e660a098571
itgsod-jannike-rivelsrud <jannike.rivelsrud@elev.it-gymnasiet.se>
2013-11-19 19:23:13 +0100

    dropdown hover menu and buttons.

--- public/dropdown_menu.js
+++ public/dropdown_menu.js
@@ -1,11 +1,30 @@
 $(document).ready(function () {
-    $(".hoverli").hover(
+    $(".hoverli1").hover(
         function () {
-            $('ul.file_menu').slideDown('medium');
+            $('ul.file_menu1').slideDown('medium');
         },
         function () {
-            $('ul.file_menu').slideUp('medium');
+            $('ul.file_menu1').slideUp('medium');
         }
     );
+});
 
+$(document).ready(function () {
+    $(".hoverli2").hover(
+        function () {
+            $('ul.file_menu2').slideDown('medium');
+        },
+        function () {
+            $('ul.file_menu2').slideUp('medium');
+        }
+    );
+//
+//    $(".file_menu li").hover(
+//        function () {
+//            $(this).children("ul").slideDown('medium');
+//        },
+//        function () {
+//            $(this).children("ul").slideUp('medium');
+//        }
+//    );
 });
